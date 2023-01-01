29332a9438149f997ba02f385a731a7e19798712
Abir Tasrif Anto <abirtasrif@gmail.com>
2023-01-01 22:43:01 +0600

    wip 1.1:09

---
+++ src/components/Slider.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const data = [
+  {
+    id: 1,
+    src: "https://images.pexels.com/photos/1787236/pexels-photo-1787236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Close-Up Photo of Dslr Camera",
+  },
+  {
+    id: 2,
+    src: "https://images.pexels.com/photos/1444416/pexels-photo-1444416.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Turned-on Flat Screen Smart Television Ahead",
+  },
+  {
+    id: 3,
+    src: "https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Top View Photo of Black Wireless Headphones",
+  },
+  {
+    id: 4,
+    src: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Person Holding Game Pad",
+  },
+  {
+    id: 5,
+    src: "https://images.pexels.com/photos/393047/pexels-photo-393047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Rose Gold Aluminum Case Apple Watch With White Sports Band",
+  },
+];
+
+const Slider = () => {
+  return <div className="slider">{}</div>;
+};
+
+export default Slider;
