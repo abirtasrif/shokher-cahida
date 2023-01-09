e57185407ff84bdd4a86a78593e63cf2bfe9c9db
Abir Tasrif Anto <abirtasrif@gmail.com>
2023-01-10 00:14:33 +0600

    adding cart slice

---
+++ src/features/products/cartSlice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: localStorage.getItem("cartItems")
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : {},
+  cartTotalQuantity: 0,
+  cartTotalAmount: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      // check if already in cart
+      const existedItemIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if ((existedItemIndex) => 0) {
+        state.carQuantity += 1;
+      } else {
+        //add to cart
+        const assembledItem = { ...action.payload, cartQuantity: 1 };
+        state.cartItems.push(assembledItem);
+
+        //add to local
+        localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+      }
+    },
+
+    removeFromCart(state, action) {
+      const updatedCartItems = state.cartItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+
+      state.cartItems = updatedCartItems;
+
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+  },
+});
